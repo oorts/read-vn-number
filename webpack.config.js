ceea1c7c77d21f06bde2cd4dd8221535b16dca3a
Đạt Nguyễn <nguyentuandat.vn@gmail.com>
2020-04-01 17:34:51 +0700

    update security

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,32 @@
-var path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+var path = require("path");
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
- module.exports = {
-    entry: {
-        'index': './src/index.js',
-        'index.min': './src/index.js',
-    },
-    output: {
-         path: path.resolve(__dirname, 'dist'),
-         filename: '[name].js'
-    },
-    optimization: {
-        concatenateModules: true,
-        minimize: true,
-        minimizer: [new UglifyJsPlugin({include: /\.min\.js$/})]
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: ['babel-loader']
-        }],
-     },
-     stats: {
-         colors: true
-     },
-     devtool: 'source-map'
- };
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+    "index.min": "./src/index.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js"
+  },
+  optimization: {
+    concatenateModules: true,
+    minimize: true,
+    minimizer: [new TerserPlugin({ include: /\.min\.js$/ })]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: "source-map"
+};
